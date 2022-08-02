00f4d88231376c75a9753e260089760384bef6ba
mahmoudrizk10 <mrizk607@gmail.com>
2022-08-02 18:24:31 +0200

    last change

--- route-nat.tf
+++ route-nat.tf
@@ -16,7 +16,7 @@ resource "google_compute_router_nat" "nat" {
   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
 
   subnetwork {
-    name                    = "private"
+    name                    = "restricted_subnet"
     source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
   }
 }
--- subnets.tf
+++ subnets.tf
@@ -1,10 +1,10 @@
-resource "google_compute_subnetwork" "public" {
+resource "google_compute_subnetwork" "managment_subnet" {
 name = "public"
 ip_cidr_range = "10.0.0.0/24"
 region = "us-central1"
 network = google_compute_network.task-vpc.id
 }
-resource "google_compute_subnetwork" "private" {
+resource "google_compute_subnetwork" "restricted_subnet" {
 name = "private"
 ip_cidr_range = "10.0.1.0/24"
 region = "us-central1"
